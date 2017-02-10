0dc9ba35465a666711172bd0a0ac5148a8df903c
TerrorTurtle <te14.carl.leong@umea.nti.se>
2017-02-10 10:40:05 +0100

    Test

--- src/web.java
+++ src/web.java
@@ -1,7 +1,12 @@
 /**
  * Created by leca14 on 2017-02-03.
  */
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.net.*;
 import java.io.*;
 import java.util.Scanner;
@@ -10,9 +15,11 @@ public class web {
     public static void main(String[] args) throws Exception {
         // ändrar lite
         String url;
+        String source;
         Scanner in = new Scanner(System.in);
         url = in.nextLine();
-        URL read = new URL(url);
+        URL read = new URL("http://"+url);
+        source = new String("http://" + url);
         BufferedReader link = new BufferedReader(
                 new InputStreamReader(read.openStream()));
 
